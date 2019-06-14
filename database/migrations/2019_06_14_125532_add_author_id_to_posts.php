293ab213a8b58085e592615b08e24e17ac0ce06f
Hardev Sharma <hd.sharma86@gmail.com>
2019-06-14 18:39:05 +0530

    Some minor fixes...

---
+++ database/migrations/2019_06_14_125532_add_author_id_to_posts.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAuthorIdToPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function($table) {
+            $table->integer('author_id')->after('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
